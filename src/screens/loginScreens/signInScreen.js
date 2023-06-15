9fe8d8f3e015287b64366486fd303ea40e4f15fa
bharathkumarreddyyedla <bharathreddy1689@gmail.com>
2023-06-15 20:13:13 +0530

    sign in ...

---
+++ src/screens/loginScreens/signInScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const SignInScreen = () => {
+  return <View><Text>SignInScreen</Text></View>
+};
+
+export default SignInScreen;
