cb9ffa49a3487b1eccf854280e6b3b2bd61ec8b6
Bharath Reddy <bharath.reddy@sellwhenever.com>
2023-06-18 22:13:20 +0530

    sign in and sign up...

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignInScreen from './src/screens/loginScreens/signInScreen';
+import SignUpScreen from './src/screens/loginScreens/signUpScreen';
+// 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SignInScreen/>
+      {/* <SignUpScreen/> */}
     </View>
   );
 }
--- src/screens/loginScreens/signUpScreen.js
+++ src/screens/loginScreens/signUpScreen.js
@@ -2,10 +2,10 @@ import { Input } from "@rneui/themed";
 import React from "react";
 import { Image, ScrollView, Text, View } from "react-native";
 import { Button, Icon, } from "react-native-elements";
-import { FontAwesome5 } from 'react-native-vector-icons';
-const SignInScreen = () => {
-  return ( 
-  <View style={{height:'100%',width:'100%'}}>
+
+const SignUpScreen = () => {
+  return (
+    <View style={{height:'100%',width:'100%'}}>
     <ScrollView>
 <Image source={require('../../../assets/images/leafySignInLogo.png')} style={{height:300,width:'100%'}} resizeMode='stretch'/>
 <View style={{flex:1,paddingHorizontal:20,marginTop:20}}>
@@ -13,12 +13,17 @@ const SignInScreen = () => {
 <Image source={require('../../../assets/images/leafLogo.png')} style={{height:40,width:'100%',position:'absolute',right:73,bottom:53}} resizeMode='contain'/>
 
 
-<Text style={{fontSize:24,fontWeight:'bold'}}>Sign in</Text>
-<Text style={{fontSize:12,fontWeight:'400',color:'grey'}}>Need to leafy? <Text style={{color:'black',fontWeight:'bold'}}>sign up for free</Text></Text>
+<Text style={{fontSize:24,fontWeight:'bold'}}>Sign up</Text>
 
 </View>
 <View style={{}}>
 <Input
+      placeholder='Full name'
+      placeholderTextColor={'grey'}
+      errorStyle={{ color: 'red' }}
+      errorMessage=''
+    />
+    <Input
       placeholder='Email'
       placeholderTextColor={'grey'}
       errorStyle={{ color: 'red' }}
@@ -30,10 +35,16 @@ const SignInScreen = () => {
       errorStyle={{ color: 'red' }}
       errorMessage=''
     />
-    <Text style={{fontSize:14,color:'black',fontWeight:'bold',paddingHorizontal:10}}>Forgot password? </Text>
+    <Input
+      placeholder='Confirm password'
+      placeholderTextColor={'grey'}
+      errorStyle={{ color: 'red' }}
+      errorMessage=''
+    />
+    {/* <Text style={{fontSize:14,color:'black',fontWeight:'bold',paddingHorizontal:10}}>Forgot password? </Text> */}
     <View style={{paddingHorizontal:10}}>
-    <Button title={'Sign in'} buttonStyle={{backgroundColor:'green',borderRadius:20,marginVertical:10,height:40}}/>
-    <Button title={'Sign in with Google'} icon={ <Icon name="google" type="font-awesome-5" size={20} color={'blue'} style={{marginRight:10}} />} buttonStyle={{backgroundColor:'white',borderRadius:20,marginVertical:10,height:40,borderWidth
+    <Button title={'Sign up'} buttonStyle={{backgroundColor:'green',borderRadius:20,marginVertical:10,height:40}}/>
+    <Button title={'Sign up with Google'} icon={ <Icon name="google" type="font-awesome-5" size={20} color={'blue'} style={{marginRight:10}} />} buttonStyle={{backgroundColor:'white',borderRadius:20,marginVertical:10,height:40,borderWidth
   :1,borderColor:'black'
   }} titleStyle={{color:'black'}}/>
     </View>
@@ -43,6 +54,6 @@ const SignInScreen = () => {
 
     </View>
   )
-};
+}
 
-export default SignInScreen;
+export default SignUpScreen
